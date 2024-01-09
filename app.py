96f5d18d1113e516238e0a1914912f877b8d12b3
LivePyTheCode <livepythecode@gmail.com>
2024-01-09 02:02:13 -0500

    commiting first project

---
+++ app.py
@@ -0,0 +1,9 @@
+character_name = "William"
+character_age = "50"
+
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+
+character_name = "Billy"
+print("He liked to be called " + character_name + ",")
+print("but didn't like being " + character_age + ".")
